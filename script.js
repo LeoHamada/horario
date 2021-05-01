82a3cf2e7b0de2d489672e04f51788ca08b0099b
LeoHamada <81055164+LeoHamada@users.noreply.github.com>
2021-04-30 23:00:45 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+function carregar() {
+    var msg = document.getElementById('msg')
+    var foto = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 0 &&  hora < 12) {
+        foto.src = 'manha.png'
+        msg.innerText += ', Bom dia!'
+        document.body.style.background = '#fed994'
+        //Bom dia!!
+    } else if (hora >= 12 && hora < 18) {
+        //boa tarde!
+        msg.innerText += ', Boa tarde!'
+        foto.src = 'tarde.png'
+        document.body.style.background = '#c7ddb5'
+    } else {
+        //boa noite!
+        msg.innerText += ', Boa noite!'
+        foto.src = 'noite.png'
+        document.body.style.background = '#17273f'
+    }
+
+}
